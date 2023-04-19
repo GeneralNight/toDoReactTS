adcf965b5cacc6b9a05383b43fa5c5bc51f9c545
Guilherme Tinen <gfmi0911@hotmail.com>
2023-04-18 22:29:27 -0300

    clean up files to start

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Header } from "./components/Header"
+import { AddTask } from "./components/AddTask"
+
+import './assets/css/global.css'
+import styles from './App.module.css'
+import { TasksStats } from "./components/TasksStats"
+import { WithoutTasks } from "./components/WithoutTasks"
+import { Task, TasksList } from "./components/TasksList"
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [tasks, setTasks] = useState<Task[]>([
+    {id:1,done: false, content: "Estudar React"},
+    {id:2,done: false, content: "Estudar React 2"},
+    {id:3,done: true, content: "Estudar React 3"},
+    {id:4,done: true, content: "Estudar React 4"},
+  ])
+
+  const handleNewTask = (task:string) => {
+    const newTask : Task = {
+      id: tasks.length + 1,
+      done: false,
+      content: task
+    }
+    setTasks([newTask, ...tasks])
+  }
+
+  const handleDeleteTask = (taskId: number) => {
+    const tasksToUpdate = tasks.filter(task => task.id !== taskId).map((task,index)=>{
+      return {
+        ...task,
+        id: index + 1
+      }
+    })
+    setTasks(tasksToUpdate)
+  }
+
+  const handleTaskState = (taskId: number) => {
+    const tasksToUpdate = tasks.map((task) => {
+      if (task.id !== taskId) return task
+      return {
+        ...task,
+        done: !task.done
+      }
+    })
+    setTasks(tasksToUpdate)
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className={styles.wrapper}>
+      <Header/>
+      <main>
+        <AddTask onCreateNewTask={(newTask:string) => handleNewTask(newTask)}/>
+        <TasksStats tasks={tasks}/>
+        {
+          !tasks.length ?
+          <WithoutTasks/> : <TasksList tasks={tasks} onDeleteTask={handleDeleteTask} onChangeTaskState={handleTaskState}/>
+        }
+      </main>
     </div>
   )
 }
